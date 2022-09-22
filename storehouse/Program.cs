843a386bb0f27039a18dbbea67f808c8c46cc6a9
Obadaalabi <obadaalabi95@gmail.com>
2022-09-22 18:18:47 +0300

    building function and page main

--- storehouse/Program.cs
+++ storehouse/Program.cs
@@ -10,30 +10,56 @@ namespace storehouse
     {
         static void Main(string[] args)
         {
-           
-            User_Controlr user_Controlr = new User_Controlr();  
-
-            
-                Console.WriteLine("welcome in aplication");
-                Console.Write("please enter the name:");
-                    var name=Console.ReadLine();
-                Console.Write("please enter the password:");
-                var password = Console.ReadLine();
-                if (user_Controlr.Authentication(name, password))
-                {
-                    Console.WriteLine("success");
-                
-                   
-                }
-                else
-                {
-                    Console.WriteLine("please check your name and password");
-                  
-                }
-
-            }
 
           
+            User_Controlr.Add_Defult();
+            Book_Controlr.Add_Defult();
+
+            Console.WriteLine("welcome in aplication");
+            Console.Write("please enter the name:");
+            var name = Console.ReadLine();
+            Console.Write("please enter the password:");
+            var password = Console.ReadLine();
+            if (User_Controlr.Authentication(name, password))
+            {
+                Console.WriteLine("success");
+                Book_Controlr.Numberofbooks();
+                bool isopen=true;
+                while (isopen)
+                {
+                    Console.WriteLine("Command List:\n" +
+                                       "1- Click (1) to view library information\n" +
+                                       "2- Press (2) to display the information of a specific book\n" +
+                                       "3- Press (3) to delete a book\n" +
+                                       "4- Press (4) to modify the title and price of a book\n" +
+                                       "5- Press (5) to exit");
+
+                    Console.Write("Enter a number from the list please    ");
+                    var select = Console.ReadLine();
+
+                    switch (select)
+                    {
+                        case ("1"): Book_Controlr.Show_information();
+                            break;
+                        case ("2"): Book_Controlr.Get_Data();
+                            break;
+                        case ("3"): Book_Controlr.Delete();
+                            break;
+                        case ("4"):
+                            break;
+                        case ("5"): isopen=false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Console.WriteLine("Good-bye");
+            }
+            else
+            {
+                Console.WriteLine("please check your name and password");
+
+            }
         }
     }
 }
--- storehouse_control/User_Controlr.cs
+++ storehouse_control/User_Controlr.cs
@@ -1,50 +1,64 @@
-﻿using System.Text.Json;
+﻿
 using storehouse_Data;
 using storehouse_Domain;
-
+using System.Text.Json;
 
 namespace storehouse_control
 {
-    public class User_Controlr : Controlr
+    public class User_Controlr 
     {
-        public override void Add()
-        {
-           
-        }
-        public bool Authentication(string name,string password)
-        {
-            return false;
-        }
-
-        public override void Add_Defult()
+        public static void Add_Defult()
         {
             String contents = File.ReadAllText("user_defult.json");
             var user_defult = JsonSerializer.Deserialize<List<User>>(contents);
             using (var context = new Storehouse_DBcontext())
             {
-                var user=context.Users.ToList();
+                var user = context.Users.ToList();
                 if (user.Count == 0)
                 {
                     context.Users.AddRange(user_defult);
                     context.SaveChanges();
                 }
-               
+
             }
+        }
+
+        public static bool Authentication(string name,string password)
+        {
+            using (var context = new Storehouse_DBcontext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.name_user == name && u.pass_word == password);
+              
+                if (user!=null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }         
+        }
+
+    
+     
+        public static void Add()
+        {
 
         }
 
-        public override void Delete()
+        public static void Delete()
         {
             
         }
 
 
-        public override void Get_Data()
+        public static void Get_Data()
         {
            
         }
 
-        public override void UpDate()
+        public static void UpDate()
         {
            
         }
