32809d8c70ece064329e14fcf7428bf9e7527244
Obadaalabi <obadaalabi95@gmail.com>
2022-09-23 04:28:42 +0300

    add_logger and file log txt

---
+++ LoggerApp/ExtantionLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerApp
+{
+    public static class ExtantionLogger
+    {
+        public static void LogErorr(this Ilogger logger, string message) 
+        {
+            logger.log(message, "Erorr");
+        }
+        public static void LogWarning(this Ilogger logger, string message)
+        {
+            logger.log(message, "Warning");
+        }
+        public static void LogInformation(this Ilogger logger, string message)
+        {
+            logger.log(message, "Information");
+        }
+    }
+}
---
+++ LoggerApp/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerApp
+{
+    public class Logger : Ilogger
+    {
+        public void log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter("log.txt",true))
+            {
+              
+                streamWriter.WriteLine(message);
+            }
+            Console.WriteLine(message);
+        }
+
+        public void log(string message, String messagetype)
+        {
+            log($"{messagetype} : {message}");
+        }
+    }
+}
